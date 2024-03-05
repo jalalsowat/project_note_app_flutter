7f9b865ad5c78a895ad1989b96a020dd57470569
jalalsowat <jalalalsowat@gmail.com>
2024-03-05 14:43:16 +0300

    Add card

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -15,6 +15,44 @@ class NotesViewBody extends StatelessWidget {
             height: 30,
           ),
           CustomAppBar(),
+          NoteItem()
+        ],
+      ),
+    );
+  }
+}
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.yellow, borderRadius: BorderRadius.circular(16)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              "Flutter Tips",
+              style: TextStyle(color: Colors.black),
+            ),
+            subtitle: const Text(
+              "Build your career with Tham",
+              style: TextStyle(color: Colors.black),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                )),
+          ),
+          const Text(
+            "May 21 , 2022",
+            style: TextStyle(color: Colors.black),
+          )
         ],
       ),
     );
