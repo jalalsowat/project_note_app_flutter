53307f6e7d42dd6459e96f89d7788f5750c93309
jalalsowat <jalalalsowat@gmail.com>
2024-03-06 15:10:43 +0300

    create custom note item

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
+import 'custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({Key? key}) : super(key: key);
@@ -16,43 +17,6 @@ class NotesViewBody extends StatelessWidget {
           ),
           CustomAppBar(),
           NoteItem()
-        ],
-      ),
-    );
-  }
-}
-
-class NoteItem extends StatelessWidget {
-  const NoteItem({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: Colors.yellow, borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              "Flutter Tips",
-              style: TextStyle(color: Colors.black),
-            ),
-            subtitle: const Text(
-              "Build your career with Tham",
-              style: TextStyle(color: Colors.black),
-            ),
-            trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.delete,
-                  color: Colors.black,
-                )),
-          ),
-          const Text(
-            "May 21 , 2022",
-            style: TextStyle(color: Colors.black),
-          )
         ],
       ),
     );
