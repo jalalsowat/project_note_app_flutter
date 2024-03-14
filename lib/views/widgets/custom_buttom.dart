f21f110f86079660dd867ce875f661270cecbaa1
jalalsowat <jalalalsowat@gmail.com>
2024-03-14 14:29:18 +0300

    validate data form

--- lib/views/widgets/custom_buttom.dart
+++ lib/views/widgets/custom_buttom.dart
@@ -3,20 +3,25 @@ import 'package:flutter/material.dart';
 import '../../constants.dart';
 
 class CustomButtom extends StatelessWidget {
-  const CustomButtom({Key? key}) : super(key: key);
+  const CustomButtom({Key? key, this.onTap}) : super(key: key);
+
+  final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 55,
-      decoration: BoxDecoration(
-          color: kPrimaryColor, borderRadius: BorderRadius.circular(8)),
-      child: const Center(
-        child: Text(
-          "Add",
-          style: TextStyle(
-              color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        decoration: BoxDecoration(
+            color: kPrimaryColor, borderRadius: BorderRadius.circular(8)),
+        child: const Center(
+          child: Text(
+            "Add",
+            style: TextStyle(
+                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
